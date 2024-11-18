fece67d3b26b65dae43dbf0ac9aa2a8086b7bc0e
sarahguii <31029131+sarahguii@users.noreply.github.com>
2024-11-18 22:14:17 +0800

    Create part2_query2__conversion_rate.sql

---
+++ part2_query2__conversion_rate.sql
@@ -0,0 +1,20 @@
+{{ config(
+    alias='conversion_rate',
+    materialized='view'
+) }}
+  
+SELECT
+    fp.experiment_variant_id,
+    u.country,
+    u.device_type,
+    -- SUM(fp.num_orders) AS total_orders,
+    -- COUNT(DISTINCT fp.user_id) AS total_participants,
+    ROUND(
+        (SUM(fp.num_orders) / NULLIF(COUNT(DISTINCT fp.user_id), 0)) * 100,
+        2
+    ) AS conversion_rate
+FROM
+    {{ ref('data__fact_participants') }} AS fp
+LEFT JOIN
+    {{ ref('data__dim_users') }} AS u ON fp.user_id = u.user_id
+GROUP BY 1, 2, 3
